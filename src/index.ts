5a3aaeea2dfffc2af27c46f9afe7baffed3ad269
Fane Yaya <y.fane75@hotmail.fr>
2023-09-12 22:42:35 +0200

    ajout

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,16 @@
-export class Item {
-  name: string;
-  sellIn: number;
-  quality: number;
+import { Item } from './items/item';
+import { GildedRose } from './gilded-rose/gilded-rose';
 
-  constructor(name, sellIn, quality) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
-}
+const items = [
+  new Item('Aged Brie', 10, 20),
+  new Item('Backstage passes to a TAFKAL80ETC concert', 5, 15),
+  new Item('Sulfuras, Hand of Ragnaros',6, 30 ),
+];
 
-export class GildedRose {
-  items: Array<Item>;
+const gildedRose = new GildedRose(items);
 
-  constructor(items = [] as Array<Item>) {
-    this.items = items;
-  }
+// Exécutez la mise à jour de la qualité
+gildedRose.updateQuality();
 
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
-
-    return this.items;
-  }
-}
+// Affichez les résultats 
+console.log(gildedRose.items);
